0c990a9cea60a6e9b32ac6e99df1f9fcddf869b4
umatbro <umatbroo@gmail.com>
2017-10-02 21:55:44 +0200

    added pursuing vehicley

--- autonomous agents and steering/flockin/flockin.pyde
+++ autonomous agents and steering/flockin/flockin.pyde
@@ -1,15 +1,28 @@
 from vehicle import Vehicle
+from time import time
 
 v = None
+v_pursuing = None
 
 def setup():
     size(1280, 800)
     global v
+    global v_pursuing
+    
     v = Vehicle(width/2, height/2)
+    v.maxspeed = 10
+    v.maxforce = 0.5
+    v_pursuing = Vehicle(30, 30)
+    background(255)
+    v.display()
+    v_pursuing.display()
+    delay(1000)
+
 
 def draw():
-    global v
     background(255)
+    global v
+    global v_pursuing
     mouse = PVector(mouseX, mouseY)
     
     # draw an ellipse at the mouse location 
@@ -18,6 +31,11 @@ def draw():
     strokeWeight(2)
     ellipse(mouse.x, mouse.y, 48, 48)
     
-    v.seek(mouse)
+    # v.seek(mouse)
+    v.arrive(mouse)
     v.update()
     v.display()
+    
+    v_pursuing.pursuit(v)
+    v_pursuing.update()
+    v_pursuing.display()
--- autonomous agents and steering/flockin/vehicle.py
+++ autonomous agents and steering/flockin/vehicle.py
@@ -1,46 +1,67 @@
 class Vehicle(object):
     def __init__(self, x, y):
-        self.position = PVector(0, 0)
+        self.position = PVector(x, y)
         self.velocity = PVector(0, -2)
-        self.acceleration = PVector(x, y)
-        
+        self.acceleration = PVector(0, 0)
+
         self.r = 6
-        self.maxspeed = 10
-        self.maxforce = 0.5
-        
-        
+        self.maxspeed = 4
+        self.maxforce = 0.1
+
     def update(self):
         # update velocity
         self.velocity.add(self.acceleration)
         # limit speed
         self.velocity.limit(self.maxspeed)
-        
+
         # reset acceleration to 0 each cycle
         self.acceleration.mult(0)
-        
+
         self.position += self.velocity
-        
-        
+
     def applyForce(self, force):
         self.acceleration.add(force)
-        
+
     def seek(self, target):
         """
         A method that calculates a steering force towards a target
         steer = desired - velocity
         """
         desired = target - self.position
-        
+
         # scale to maximum speed
         desired.setMag(self.maxspeed)
-        
+
         # steering = desired - velocity
         steer = desired - self.velocity
         steer.limit(self.maxforce)
-        
+
         self.applyForce(steer)
-        
-        
+
+    def arrive(self, target):
+        """
+        A method that calculates a steering force towards a target
+        STEER = DESIRED - VELOCITY
+        """
+        desired = target - self.position
+        d = desired.mag()
+
+        m = map(d, 0, 100, 0, self.maxspeed) if d < 100 else self.maxspeed
+        desired.setMag(m)
+
+        steer = desired - self.velocity
+        steer.limit(self.maxforce)
+        self.applyForce(steer)
+
+    def pursuit(self, another):
+        desired = another.position - self.position
+        desired.setMag(self.maxspeed)
+
+        steer = desired - self.velocity
+        steer.limit(self.maxforce)
+
+        self.applyForce(steer)
+
     def display(self):
         theta = self.velocity.heading2D() + PI/2
         fill(127)
